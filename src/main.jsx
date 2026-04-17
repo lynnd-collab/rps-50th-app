ad171013893b9dbea4fa6d22c292f8f8d8769852
Lynn Cornell <lynncornell@gmail.com>
2026-04-17 14:04:47 -0600

    Add env var diagnostic log on startup

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+console.log('[env] VITE_SUPABASE_URL prefix:', import.meta.env.VITE_SUPABASE_URL?.slice(0, 10) ?? 'MISSING')
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
